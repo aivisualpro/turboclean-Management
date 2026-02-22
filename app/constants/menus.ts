cddd674998adf921c376d33742cd8b749db0ca60
adeel-a11y <admin@aivisualpro.com>
2026-02-22 21:46:06 +0500

    services added2

--- app/constants/menus.ts
+++ app/constants/menus.ts
@@ -2,7 +2,7 @@ import type { NavMenu, NavMenuItems } from '~/types/nav'
 
 export const navMenu: NavMenu[] = [
   {
-    heading: 'General',
+    heading: '',
     items: [
       {
         title: 'Dashboard',
@@ -24,23 +24,12 @@ export const navMenu: NavMenu[] = [
         icon: 'i-lucide-briefcase',
         link: '/services',
       },
-    ],
-  },
-  {
-    heading: 'Apps',
-    items: [
       {
         title: 'Tasks',
         icon: 'i-lucide-kanban',
         link: '/tasks',
         new: true,
       },
-
-    ],
-  },
-  {
-    heading: 'Sales & Commerce',
-    items: [
       {
         title: 'Work Orders',
         icon: 'i-lucide-file-text',
@@ -51,105 +40,6 @@ export const navMenu: NavMenu[] = [
         icon: 'i-lucide-receipt',
         link: '/sales/invoices',
       },
-
-    ],
-  },
-
-
-  {
-    heading: 'Finance & Accounting',
-    items: [
-      {
-        title: 'Accounts',
-        icon: 'i-lucide-wallet',
-        link: '/finance/accounts',
-      },
-      {
-        title: 'Transactions',
-        icon: 'i-lucide-arrow-right-left',
-        link: '/finance/transactions',
-      },
-      {
-        title: 'Expenses',
-        icon: 'i-lucide-credit-card',
-        link: '/finance/expenses',
-      },
-      {
-        title: 'Tax Management',
-        icon: 'i-lucide-percent',
-        link: '/finance/taxes',
-      },
-      {
-        title: 'Balance Sheet',
-        icon: 'i-lucide-landmark',
-        link: '/finance/balance-sheet',
-        new: true,
-      },
-      {
-        title: 'Income Statement',
-        icon: 'i-lucide-receipt',
-        link: '/finance/income-statement',
-        new: true,
-      },
-      {
-        title: 'Financial Ratios',
-        icon: 'i-lucide-chart-no-axes-combined',
-        link: '/finance/ratios',
-        new: true,
-      },
-      {
-        title: 'Business Health',
-        icon: 'i-lucide-heart-pulse',
-        link: '/finance/business-health',
-        new: true,
-      },
-    ],
-  },
-
-  {
-    heading: 'Support',
-    items: [
-      {
-        title: 'Tickets',
-        icon: 'i-lucide-ticket',
-        link: '/support/tickets',
-      },
-      {
-        title: 'Knowledge Base',
-        icon: 'i-lucide-book-open',
-        link: '/support/knowledge-base',
-      },
-      {
-        title: 'Live Chat',
-        icon: 'i-lucide-message-circle',
-        link: '/support/chat',
-        new: true,
-      },
-    ],
-  },
-  {
-    heading: 'Marketing',
-    items: [
-      {
-        title: 'Campaigns',
-        icon: 'i-lucide-megaphone',
-        link: '/marketing/campaigns',
-      },
-      {
-        title: 'Email Blasts',
-        icon: 'i-lucide-mails',
-        link: '/marketing/email-blasts',
-      },
-      {
-        title: 'Analytics',
-        icon: 'i-lucide-bar-chart-3',
-        link: '/marketing/analytics',
-      },
-    ],
-  },
-  {
-    heading: 'Reports',
-    items: [
       {
         title: 'Sales Reports',
         icon: 'i-lucide-trending-up',
